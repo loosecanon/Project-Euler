8841e552d5b1970c7e533daa8a823d39b298afcc
Ben Canon <admin@loosecanon.net>
2012-11-23 11:14:20 -0500

    Much faster solution for problem 3
    Removed prime checking and implemented better bounding on the issue

--- problem3.py
+++ problem3.py
@@ -1,18 +1,12 @@
 import math
 num = 600851475143
 largest = 1 # largest factor found in search space
-for i in range(1, math.floor(math.sqrt(num))):
-    for x in range(2,i):
-        if i%x == 0:
-            break
-    else:
-        #print('found new prime:')
-        #print(i)
-        if num%i == 0:
-            largest = i
-            num = num/largest
-            print('found new factor: ')
-            print(i)
+for i in range(1, math.floor(math.sqrt(600851475143))):
+    if num%i == 0:
+        largest = i
+        num = num/largest
+        print('found new factor: ')
+        print(i)
 
 print('largest factor: ')
 print(largest)
