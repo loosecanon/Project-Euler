c6bcaf01223549f3861418cbda3b3ab1cb361f46
Ben Canon <admin@loosecanon.net>
2012-12-08 13:51:26 -0500

    Cleaned up problem 13 solution
    Removed unused imports, cleaner formatting

--- Problem13.py
+++ Problem13.py
@@ -1,14 +1,10 @@
-import math
-from uuid import int_
-from lib2to3.fixer_util import Number
-
 numberArray = [[]]
 textFile = open("numbers.txt","r")
 tmp = textFile.readline()
 lines = 0
 #read in numbers
 while tmp:
-    print("tmp = ", tmp)
+    #print("tmp = ", tmp)
     i = 0
     for char in tmp:
         numberArray[lines].append([])
@@ -25,14 +21,13 @@ for i in range(0,50):
     subTotal = carry
     for j in range(0,100):
         #print("i = ", i, "number = ", numberArray[j][49-i])
-        from ctypes.wintypes import INT
         subTotal += int(numberArray[j][49-i])
     print("subtotal = ", subTotal)
     d,r = divmod(subTotal, 10)
     results.append("")
     results[i] = r
     carry = d
-    print("r = ", r, " carry = ", carry)
+    #print("r = ", r, " carry = ", carry)
 while carry > 0:
     i+=1
     d,r = divmod(carry, 10)
